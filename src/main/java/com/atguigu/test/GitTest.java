b2cdfc6c0de7273996b5c8a365f215102b03f723
王睿 <16602155159@163.com>
2021-01-18 17:24:45 +0800

    添加master输出

--- src/main/java/com/atguigu/test/GitTest.java
+++ src/main/java/com/atguigu/test/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("222222");
         System.out.println("333333");
         System.out.println("hotfix");
+        System.out.println("master");
     }
 }
