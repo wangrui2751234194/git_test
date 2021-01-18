957d78d5d20d845cce7a3afd00bb0344afa9ecb6
王睿 <16602155159@163.com>
2021-01-18 17:29:07 +0800

    添加hotfix输出

--- src/main/java/com/atguigu/test/GitTest.java
+++ src/main/java/com/atguigu/test/GitTest.java
@@ -5,6 +5,6 @@ public class GitTest {
         System.out.println("111111");
         System.out.println("222222");
         System.out.println("333333");
-        System.out.println("hotfix");
+        System.out.println("hotfix11");
     }
 }
