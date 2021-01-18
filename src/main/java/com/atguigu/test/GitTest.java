469a4f6ee3f2b112f6d1a0f637d6a16fe5dbf4ce
王睿 <16602155159@163.com>
2021-01-18 17:19:02 +0800

    创建项目

---
+++ src/main/java/com/atguigu/test/GitTest.java
@@ -0,0 +1,9 @@
+package com.atguigu.test;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("111111");
+        System.out.println("222222");
+        System.out.println("333333");
+    }
+}
